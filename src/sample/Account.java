846666d125ce637888d9353f62d9b81685e883ec
Doroshenk <rtdoroshenk@liberty.edu>
2020-04-11 14:38:42 -0400

    Updated Account Class

--- src/sample/Account.java
+++ src/sample/Account.java
@@ -1,4 +1,32 @@
 package sample;
 
+import java.math.BigDecimal;
+
 public class Account {
+
+    private final int accountID;
+    private String name;
+    private BigDecimal balance;
+
+    public Account(int accountID, String name, BigDecimal balance) {
+        this.accountID = accountID;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
